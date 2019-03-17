528129d03f10020a574570f797468be005c7ecb8
NegarMirgati <negar.nbj95@gmail.com>
2019-03-17 16:59:08 +0330

    Add CORSfilter class

---
+++ src/main/java/Servlets/Filters/CorsFilter.java
@@ -0,0 +1,46 @@
+package Servlets.Filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ WebFilter(asyncSupported = true, urlPatterns = { "/*" })
+public class CorsFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        System.out.println("CORSFilter HTTP Request: " + request.getMethod());
+
+        // Authorize (allow) all domains to consume the content
+        ((HttpServletResponse) resp).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) resp).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+
+        HttpServletResponse response = (HttpServletResponse) resp;
+
+        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+        // pass the request along the filter chain
+        chain.doFilter(request, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
