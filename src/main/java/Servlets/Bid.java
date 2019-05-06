02c9952319de868dc439cc73c195537ec7fb28d4
setareA <setareaskari79@gmail.com>
2019-05-06 23:43:34 +0430

    implement Bid

--- src/main/java/Servlets/Bid.java
+++ src/main/java/Servlets/Bid.java
@@ -8,9 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import Commands.*;
 import ContentProviders.ProjectContentProvider;
+import DataLayer.DataMappers.Project.BidMapper;
 import Exceptions.BidAlreadyDoneException;
 import Exceptions.ProjectAccessForbiddenException;
 import Exceptions.ProjectNotFoundException;
@@ -34,41 +36,50 @@ public class Bid extends HttpServlet {
         JSONObject instance = new JSONObject();
 
         try {
-            BidCommand bidCommand = new BidCommand(projectID, Integer.valueOf(bidAmount), "1");
+            //BidCommand bidCommand = new BidCommand(projectID, Integer.valueOf(bidAmount), "1");
             response.setContentType("application/json;charset=UTF-8");
-            ProjectContentProvider.hasBadeForProject("1", projectID);
-            ProjectContentProvider.checkAccess("1", projectID);
+           // ProjectContentProvider.hasBadeForProject("1", projectID);
+            BidMapper bm = new BidMapper();
+            if ( bm.hasBade(projectID,"1",Integer.valueOf(bidAmount))){
+                throw new BidAlreadyDoneException("bid already done");
+            }
+            //ProjectContentProvider.checkAccess("1", projectID);
 
-            if(!bidCommand.bidIsPossible()){
+            if(bm.biddingPossible(projectID,Integer.valueOf(bidAmount)) == false){
                 response.setStatus(response.SC_BAD_REQUEST);
                 JSONObject status = new JSONObject();
                 status.put("status", "Invalid bid amount");
                 out.println(status);
             }
             else{
-                bidCommand.execute();
+                //bidCommand.execute();
+                bm.fillTable(projectID,"1",Integer.valueOf(bidAmount));
                 response.setStatus(response.SC_OK);
                 JSONObject status = new JSONObject();
                 status.put("status", "bid successfully done.");
                 out.println(status);
 
             }
-        } catch (UserNotFoundException | ProjectNotFoundException e) {
-            printApiOutputError(e, 404,response);
+        }
+//        catch (UserNotFoundException | ProjectNotFoundException e) {
+//            printApiOutputError(e, 404,response);
 
-        } catch (BidAlreadyDoneException e) {
+     //   }
+        catch (BidAlreadyDoneException e) {
             printApiOutputError(e, 409,response);
         }
 
-        catch (ProjectAccessForbiddenException e){
-            printApiOutputError(e, 403,response);
-        }
+//        catch (ProjectAccessForbiddenException e){
+//            printApiOutputError(e, 403,response);
+//        }
 
         catch(NumberFormatException e){
             instance.put("status", 422);
             instance.put("message", "Invalid parameter type exception");
             response.setStatus(422);
             out.println(instance);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
     }
