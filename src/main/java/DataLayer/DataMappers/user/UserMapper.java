00f890bf6ac202d997dfb36491951fc3acc8d7ed
NegarMirgati <negar.nbj95@gmail.com>
2019-05-06 09:44:27 +0430

    add fill table to userMapper

--- src/main/java/DataLayer/DataMappers/user/UserMapper.java
+++ src/main/java/DataLayer/DataMappers/user/UserMapper.java
@@ -11,14 +11,127 @@ public class UserMapper extends Mapper<User, String> implements IUserMapper{
     private static final String COLUMNS = "username, firstName, lastName, jobTitle, profilePictureURL, bio";
 
     public UserMapper() throws SQLException {
+        System.out.println("here for user Table!!");
         Connection con = DBCPDBConnectionPool.getConnection();
         Statement st =
                 con.createStatement();
         st.executeUpdate("CREATE TABLE IF NOT EXISTS " + "user" + " " + "(username TEXT PRIMARY KEY, firstName TEXT," +
                 " lastName TEXT, jobTitle TEXT, profilePictureURL TEXT, bio TEXT)");
 
+        try {
+            fillTable(con);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
         st.close();
         con.close();
+        System.out.println("End!!");
+    }
+
+    private static User getUser1Data(){
+        String bio = "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.\n" +
+                "          چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.\n" +
+                " ";
+        String proLink  = "https://static.yeklist.com/kala/images/1/p-96.jpg";
+        Skill s = new  Skill("HTML", 5);
+        Skill s1 = new Skill("Javascript", 4);
+        Skill s2 = new Skill("C++", 2);
+        Skill s3 = new Skill("Java", 3);
+        HashMap<String, Skill> map = new HashMap<String, Skill>();
+        map.put("HTML", s);
+        map.put("Javascript", s1);
+        map.put("C++", s2);
+        map.put("Java", s3);
+        return new User("1", "پسر", "عمه زا","خارخاسک", proLink, map, bio);
+
+    }
+
+    private static User getUser2Data(){
+        String bio = "در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.";
+        Skill s = new  Skill("Linux", 5);
+        Skill s1 = new Skill("SEO", 4);
+        Skill s2 = new Skill("C", 2);
+        Skill s3 = new Skill("Java", 3);
+        HashMap<String, Skill> map = new HashMap<String, Skill>();
+        map.put("Linux", s);
+        map.put("SEO", s1);
+        map.put("C", s2);
+        map.put("Java", s3);
+        return new User("2", "فامیل", "دور","پدر نمونه", "http://cdn-tehran.wisgoon.com/dlir-s3/10531477474534427630.jpeg",map, bio);
+    }
+
+    private static User getUser3Data(){
+        System.out.println("adding user");
+        String bio = "کلاه\u200Cقرمزی نام شخصیت عروسکی ایرانی است که توسط ایرج طهماسب و حمید جِبِلّی خلق و توسط مرضیه محبوب طراحی و ساخته شده\u200Cاست. فیلم\u200Cهای ساخته\u200Cشده براساس این شخصیت از پرفروش\u200Cترین فیلم\u200Cهای تاریخ سینمای ایران است.[۱] کلاه\u200Cقرمزی با بیش از دو دهه فعالیت در عرصهٔ سینما و تلویزیون، از یک عروسک به پدیده\u200Cای فرهنگی تبدیل شد که توانست مخاطبان بسیاری در بین نسل\u200Cهای گوناگون به\u200Cویژه متولدین دههٔ ۱۳۶۰ بیابد.";
+        Skill s = new  Skill("HTML", 5);
+        Skill s1 = new Skill("C", 6);
+        Skill s2 = new Skill("Photoshop", 10);
+
+        HashMap<String, Skill> map = new HashMap<String, Skill>();
+        map.put("HTML", s);
+        map.put("C", s1);
+        map.put("Photoshop", s2);
+
+        return new User("3", "پسر", "خاله","نونوا", "http://www.soonami.ir/wp-content/uploads/49384726_617182022059223_729630059174640160_n.jpg",map, bio);
+
+    }
+
+    private static void fillTable(Connection con) throws SQLException {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(getUser1Data());
+        users.add(getUser2Data());
+        users.add(getUser3Data());
+        System.out.println("adding userssssssss");
+        ArrayList<String> attrs = getAttrs();
+        for (int i = 0; i < users.size(); i++){
+            HashMap<String, String> userData = new HashMap<>();
+
+            userData.put("username", users.get(i).getUsername());
+            userData.put("firstName", users.get(i).getFirstName());
+            userData.put("lastName", users.get(i).getLastName());
+            userData.put("jobTitle", users.get(i).getJobTitle());
+            userData.put("profilePictureURL", users.get(i).getProfilePictureURL());
+            userData.put("bio", users.get(i).getBio());
+            addToTable(con, "user", userData, attrs);
+
+        }
+
+    }
+
+    private static ArrayList<String> getAttrs(){
+        ArrayList<String> attrs = new ArrayList<String>(
+                Arrays.asList("username",
+                "firstName",
+                "lastName",
+                "jobTitle",
+                "profilePictureURL",
+                "bio"));
+        return attrs;
+    }
+
+    public static void addToTable(Connection con,String tableName, HashMap<String, String> userData, ArrayList<String> attrs) throws SQLException {
+        String sqlCommand = insertCommand(tableName, attrs);
+        PreparedStatement prp = con.prepareStatement(sqlCommand);
+        for(int j = 1; j <= attrs.size(); j++)
+            prp.setString(j, userData.get(attrs.get(j)));
+        prp.executeUpdate();
+        prp.close();
+    }
+
+
+    private static String insertCommand(String tableName, ArrayList<String> attributes){
+        String sqlCommand = "INSERT INTO " + tableName + "(";
+        for(String attr: attributes)
+            sqlCommand += attr + ",";
+        sqlCommand = sqlCommand.substring(0, sqlCommand.length()-1);
+        sqlCommand += ") VALUES(";
+        for(int i = 0; i < attributes.size(); i++)
+            sqlCommand += "?,";
+        sqlCommand = sqlCommand.substring(0, sqlCommand.length()-1);
+        sqlCommand += ");";
+        System.out.println("sql cmd is: " + sqlCommand);
+        return sqlCommand;
     }
 
     @Override
