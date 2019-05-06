3ae92d88b3431c841b4829706c619373484d1ab6
NegarMirgati <negar.nbj95@gmail.com>
2019-05-06 22:33:41 +0430

    implement dellSkill

--- src/main/java/DataLayer/DataMappers/IMapper.java
+++ src/main/java/DataLayer/DataMappers/IMapper.java
@@ -3,10 +3,7 @@ package DataLayer.DataMappers;
 import java.sql.SQLException;
 
 public interface IMapper<T, I> {
-<<<<<<< HEAD
     T find(I id) throws SQLException;
-=======
-
-    T  find(I id) throws SQLException;
->>>>>>> a1bf3003c83c99a1fff2b864ea5bb5b5e931a539
 }
+
+
