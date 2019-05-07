f2dfa0d5b13fb7f7a7b7e2ebf4f4801bad5ef263
NegarMirgati <negar.nbj95@gmail.com>
2019-05-07 20:08:51 +0430

    connect user search to server

--- react-client/src/Home/SearchUsers.tsx
+++ react-client/src/Home/SearchUsers.tsx
@@ -11,26 +11,66 @@ export default class SearchUsers extends Component<any, State> {
       id : ""
     };
   }
+
+  setDataToState = (response : any) => {
+    var users : any[] = [];
+    if(response.data.length != 0){
+      var key = Object.keys(response.data[0])[0]
+      for(var i = 0; i < response.data.length; i++){
+        var key = Object.keys(response.data[i])[0]
+        console.log('here', response.data[i][key])  
+        var link = "http://localhost:3000/user?id=" + response.data[i][key].id;
+        var user = {
+          id : response.data[i][key].id,
+          name : response.data[i][key].name,
+          lastname : response.data[i][key].lastname,
+          proLink : response.data[i][key].proLink,
+          jobTitle : response.data[i][key].jobTitle,
+          linkToProPage  : link   
+        } 
+        console.log('u', user)
+        users.push(user);
+      }
+    }
+    this.setState({ users: users});
+}
+
   componentDidMount(){
-    var link = 'http://localhost:8080/users'
+    var link = 'http://localhost:8080/users?q='
     axios.get(link)
     .then((response : any) => {
-        this.setState({users : response.data});
-        }
-      )
+        this.setDataToState(response)
+        })
       .catch(function (error : any) {
         console.log(error)
           toast.error('خطا در دریافت کاربران');
       })
   }
 
+  handleChangeinput = (event : any) => {
+    var link = 'http://localhost:8080/users?q='
+    link += event.target.value;
+    console.log('searching for ' + link);
+    axios.get(link)
+    .then((response : any) => {
+      this.setDataToState(response)
+    })
+      .catch(function (error : any) {
+        console.log(error)
+          toast.error('خطا در دریافت کاربران');
+      })
+
+  }
+
   getUserCards = () => {
     var userCardsJSX : JSX.Element[] = [];
     var numUsers = this.state.users.length;
-    console.log(numUsers);
-
     for(var i = 0; i < numUsers ; i = i + 1){
-      userCardsJSX.push(<UserCard {...this.state.users[i]}/>);
+
+      var key = Object.keys(this.state.users[i])[0]
+      console.log('geteeeee', this.state.users[i][key])     
+      userCardsJSX.push(<UserCard id = {this.state.users[i][key].id}  {...this.state.users[i]}/>); 
+      
     }
     return userCardsJSX;
   }
@@ -41,7 +81,7 @@ export default class SearchUsers extends Component<any, State> {
             <div className = "row">
               <div className = "search-container-user">
                 <form action = "">
-                  <input type="text" placeholder="جستجو نام کاربر" name="search"/>
+                  <input onChange = {this.handleChangeinput} type="text" placeholder="جستجو نام کاربر" name="search"/>
                 </form>
               </div>
             </div>
--- react-client/src/Home/UserCard.tsx
+++ react-client/src/Home/UserCard.tsx
@@ -12,8 +12,8 @@ export default class UserCard extends Component<any, State> {
             linkToProPage : ""
         };
       }
+    
     componentDidMount(){
-       
         var key = Object.keys(this.props)[0]
         console.log('here', this.props[key])        
         this.setState({id: this.props[key].id});
@@ -23,19 +23,31 @@ export default class UserCard extends Component<any, State> {
         this.setState({proLink : this.props[key].proLink});
         var link = "http://localhost:3000/user?id=" + this.props[key].id;
         this.setState({linkToProPage : link});
-        console.log(this.state)
+        console.log('dddd', this.state)
+    }
+    componentWillReceiveProps(){
+        /*var key = Object.keys(this.props)[0]
+        console.log('here', this.props[key])        
+        this.setState({id: this.props[key].id});
+        this.setState({name : this.props[key].name});
+        this.setState({lastname : this.props[key].lastname});
+        this.setState({jobTitle : this.props[key].jobTitle});
+        this.setState({proLink : this.props[key].proLink});
+        var link = "http://localhost:3000/user?id=" + this.props[key].id;
+        this.setState({linkToProPage : link});
+        console.log('uuuu', this.state)*/
     }
   render() {
     return (
         <div className = "row">
             <div className = "search-user">
-                <a id = "userCard-a" href = {this.state.linkToProPage}>
+                <a id = "userCard-a" href = {this.props.linkToProPage}>
                     <div className = "col">
-                        <img className="img-rounded search-image img-rounded img-responsive" src = {this.state.proLink} alt = "image" />
+                        <img className="img-rounded search-image img-rounded img-responsive" src = {this.props.proLink} alt = "image" />
                     </div>
                     <div className = "col">
-                        <p className = "search-user-name"> {this.state.name} {' '} {this.state.lastname} </p>
-                        <p className = "search-user-job">{this.state.jobTitle}</p>
+                        <p className = "search-user-name"> {this.props.name} {' '} {this.state.lastname} </p>
+                        <p className = "search-user-job">{this.props.jobTitle}</p>
                     </div>
                 </a>
             </div>
