cae164868965d4dadee278b58e8a9443448bdc48
setareA <setareaskari79@gmail.com>
2019-05-17 13:58:22 +0430

    sending jwt of logged user

--- react-client/src/User/UserComponentLogged.tsx
+++ react-client/src/User/UserComponentLogged.tsx
@@ -1,91 +1,92 @@
-import React, { Component, ReactNode } from 'react'
-const axios = require('axios');
-import LoadingOverlay from 'react-loading-overlay';
-import { toast } from 'react-toastify';
-import $ from 'jquery'; 
-import UserCommon from './UserCommon'
+import React, { Component, ReactNode } from "react";
+const axios = require("axios");
+import LoadingOverlay from "react-loading-overlay";
+import { toast } from "react-toastify";
+import $ from "jquery";
+import UserCommon from "./UserCommon";
 import Footer from "../Common/Footer";
 import Header from "../Common/Header";
-import SkillsLogged from './SkillsLogged';
-import Loader from '../Common/Loader'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'src/Styles/style.css'
-import 'src/Styles/userLoggedStyle.css'
+import SkillsLogged from "./SkillsLogged";
+import Loader from "../Common/Loader";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "src/Styles/style.css";
+import "src/Styles/userLoggedStyle.css";
 
-export default class UserComponentLogged extends Component<any,  State> {
-
-  constructor(props : any) {
+export default class UserComponentLogged extends Component<any, State> {
+  constructor(props: any) {
     super(props);
     this.state = {
-      id : "",
-      name : "",
-      lastname : "",
-      job : "",
-      bio : "",
-      proLink : "",
-      skills : [],
-      possibleSkills : [],
-      loading : true
+      id: "",
+      name: "",
+      lastname: "",
+      job: "",
+      bio: "",
+      proLink: "",
+      skills: [],
+      possibleSkills: [],
+      loading: true
     };
   }
-  
-  componentDidMount(){
-    var linktmp = 'http://localhost:8080/user?id='
-    if(this.props.userId){
-      var  link = linktmp.concat(this.props.userId)
-      axios.get(link)
-      .then((response : any) => {
-        let obj: any = JSON.parse(JSON.stringify(response.data));
-        this.setState({name : obj["name"]});
-        this.setState({lastname : obj["lastname"]});
-        this.setState({id : obj["id"]});
-        this.setState({job : obj["jobTitle"]});
-        this.setState({bio : obj["bio"]});
-        this.setState({proLink : obj["proLink"]});
-        this.setState({skills : obj["skills"]});
-        this.setState({possibleSkills : obj["possibleSkills"]});
-        this.setState({loading : false})
-      })
-      .catch(function (error : any) {
-        toast.error('خطا در برقراری ارتباط با سرور');
-      })
+
+  componentDidMount() {
+    if (this.props.userId) {
+      var link = "http://localhost:8080/user?id=".concat(this.props
+        .userId as string);
+      var config = {
+        headers: { Authorization: "bearer " + localStorage.getItem("jwt") }
+      };
+      axios
+        .get(link, config, config)
+        .then((response: any) => {
+          let obj: any = JSON.parse(JSON.stringify(response.data));
+          this.setState({ name: obj["name"] });
+          this.setState({ lastname: obj["lastname"] });
+          this.setState({ id: obj["id"] });
+          this.setState({ job: obj["jobTitle"] });
+          this.setState({ bio: obj["bio"] });
+          this.setState({ skills: obj["skills"] });
+          this.setState({ proLink: obj["proLink"] });
+          this.setState({ possibleSkills: obj["possibleSkills"] });
+        })
+        .catch(function(error: any) {
+          toast.error("اتصال با سرور با خطا مواجه شد");
+        })
+        .then(() => {
+          this.setState({ loading: false });
+          console.log("state", this.state);
+        });
     }
   }
 
-  getMainElements(){
-    if(this.state.loading){
-      return(<Loader/>);
-    }
-    else{
-      return(
-      <div id = "fill-view-point">
-        <Header/>
+  getMainElements() {
+    if (this.state.loading) {
+      return <Loader />;
+    } else {
+      return (
+        <div id="fill-view-point">
+          <Header />
           <UserCommon {...this.state} />
-          <SkillsLogged {...this.state}/> 
-        <Footer/>
-      </div>)
+          <SkillsLogged {...this.state} />
+          <Footer />
+        </div>
+      );
     }
   }
 
-  render(){
-    document.body.classList.add('htmlBodyStyle');
-    return (
-      <div>
-        {this.getMainElements()}
-      </div>
-    )
+  render() {
+    document.body.classList.add("htmlBodyStyle");
+    return <div>{this.getMainElements()}</div>;
   }
 }
 
-interface State{
-  id : ""
-  name : "",
-  lastname : "",
-  job : "",
-  bio : "",
-  proLink : "",
-  skills : any[],
-  possibleSkills : any[],
-  loading
+interface State {
+  id: "";
+  name: "";
+  lastname: "";
+  job: "";
+  bio: "";
+  proLink: "";
+  skills: any[];
+  possibleSkills: any[];
+  loading;
 }
-
